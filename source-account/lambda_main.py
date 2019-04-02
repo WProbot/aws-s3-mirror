aaf4c647ea3b2a0789461ba120161b32a91db70b
Michael Ludvig <mludvig@logix.net.nz>
2019-04-02 15:01:09 +1300

    Added Delete support

--- source-account/lambda_main.py
+++ source-account/lambda_main.py
@@ -13,16 +13,34 @@ def lambda_handler(event, context):
     print(json.dumps(event))
     for record in event['Records']:
         try:
-            assert(record['eventSource'] == 'aws:s3' and record['eventName'] == 'ObjectCreated:Put')
+            assert(record['eventSource'] == 'aws:s3')
         except:
             print("ERROR: Unexpected event record!")
             print(json.dumps(record))
             continue
 
-        source = {
-            'Bucket': record['s3']['bucket']['name'],
-            'Key': record['s3']['object']['key']
-        }
-        # copy() doesn't return anything
-        destination.copy(source, source['Key'],
-            ExtraArgs = {'ACL': 'bucket-owner-full-control'})
+        if record['eventName'] in [ 'ObjectCreated:Put', 'ObjectCreated:Copy' ]:
+            source = {
+                'Bucket': record['s3']['bucket']['name'],
+                'Key': record['s3']['object']['key']
+            }
+            # copy() doesn't return anything
+            destination.copy(source, source['Key'],
+                ExtraArgs = {'ACL': 'bucket-owner-full-control'})
+        elif record['eventName'].startswith('ObjectRemoved:'):
+            source = {
+                'Bucket': record['s3']['bucket']['name'],
+                'Key': record['s3']['object']['key']
+            }
+            ret = destination.delete_objects(
+                Delete={
+                    'Objects': [ {
+                        'Key': record['s3']['object']['key']
+                    } ]
+                }
+            )
+            print(json.dumps(ret))
+        else:
+            print("ERROR: Unexpected S3 event!")
+            print(json.dumps(record))
+            continue
